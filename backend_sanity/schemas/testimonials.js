92f5b3e55948bea01bffd624232602c5b09a347e
kartikgowda <kartik.gowda55@gmail.com>
2022-03-30 11:55:07 +1100

    Back End Files

--- backend_sanity/schemas/testimonials.js
+++ backend_sanity/schemas/testimonials.js
@@ -1,31 +1,30 @@
 export default {
-  name: `testimonials`,
-  title: `Testimonials`,
-  type: 'document',
-  fields: [
-    {
-      name: 'name',
-      title: `Name`,
-      type: 'string',
-    },
-    {
-      name: 'company',
-      title: `Company`,
-      type: 'string',
-    },
-    {
-      name: 'imageURL',
-      title: `ImgURL`,
-      type: 'image',
-      //? HOTSPOT  https://www.sanity.io/docs/presenting-images
-      options: {
-        hotspot: true,
-      },
-    },
-    {
-      name: 'feedback',
-      title: `Feedback`,
-      type: 'string',
-    },
-  ],
-};
+    name:'testimonials',
+    title:'Testimonials',
+    type: 'document',
+    fields:[
+        { 
+            name:'name',
+            title:'Name',
+            type: 'string'
+        },
+        {
+            name:'company',
+            title:'Company',
+            type:'string'
+        },
+        {
+            name:'imgurl',
+            title:'ImgUrl',
+            type: 'image',
+            options: {
+              hotspot: true,
+            },
+        },  
+        {
+            name:'feedback',
+            title:'Feedback',
+            type:'string'
+        }
+    ]
+}
