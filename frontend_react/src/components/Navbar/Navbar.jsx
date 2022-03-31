4319fe85a8253550a58ebf2439e33e15904dddea
kartikgowda <kartik.gowda55@gmail.com>
2022-03-31 16:22:21 +1100

    NavBar Animation and Toggle Button

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React,{useState} from 'react';
+
+//? Importing icons & animations for Navbar 
+import { HiMenuAlt4, HiX } from 'react-icons/hi'
+import { motion} from 'framer-motion';
 
 import { images } from '../../constants';
 import './Navbar.scss';
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
   return (
     <nav className="app__navbar">
       <div className="app__navbar-logo">
@@ -24,8 +29,32 @@ const Navbar = () => {
             <a href={`#${item}`}>{item}</a>
           </li>
         ))}
-        ;
       </ul>
+      <div className="app__navbar-menu">
+        <HiMenuAlt4 onClick={()=>setToggle(true)}/>
+        {toggle && (
+          <motion.div
+          whileInView={{x:[300,0]}}
+        transition = {{duration:0.85, ease:'easeOut'}}>
+          <HiX onClick={()=>setToggle(false)}/>
+      <ul>
+          {[
+          `home`,
+          `about`,
+          `contact`,
+          `work`,
+          `skills`,
+          `testimonials`,
+          `work`,
+        ].map((item) => (
+          <li  key={{item}}>
+            <a href={`#${item}`} onClick={()=>setToggle(false)}>{item}</a>
+          </li>
+        ))}</ul>     
+          </motion.div>
+        )
+        }
+      </div>
     </nav>
   );
 };
