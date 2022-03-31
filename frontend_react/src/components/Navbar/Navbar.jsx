f8b5079149aa1aef9b4428a5d6d56053c8c6de28
kartikgowda <kartik.gowda55@gmail.com>
2022-03-31 15:44:38 +1100

    Navigation Bar Styling

---
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import { images } from '../../constants';
+import './Navbar.scss';
+
+const Navbar = () => {
+  return (
+    <nav className="app__navbar">
+      <div className="app__navbar-logo">
+        <img src={images.logo} alt="logo" />
+      </div>
+      <ul className="app__navbar-links">
+        {[
+          `home`,
+          `about`,
+          `contact`,
+          `work`,
+          `skills`,
+          `testimonials`,
+          `work`,
+        ].map((item) => (
+          <li className="app__flex p-text" key={`link-${item}`}>
+            <div />
+            <a href={`#${item}`}>{item}</a>
+          </li>
+        ))}
+        ;
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
