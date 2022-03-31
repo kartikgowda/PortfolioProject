fc9351ba3331e3df327dd7c97dce91cb52e65cc9
kartikgowda <kartik.gowda55@gmail.com>
2022-03-31 12:48:05 +1100

    Creating App.scss and basic styles & fonts. Imported in App.js

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
+import './App.scss';
 import { About, Footer, Header, Skills, Testimonials, Work } from './container';
-
 import { Navbar } from './components';
 
 const App = () => {
