98f14633be428a75956f41b279723b1dc5dbca6f
kartikgowda <kartik.gowda55@gmail.com>
2022-03-31 14:59:18 +1100

    Import images and Setting NavBar

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-import './App.scss';
 import { About, Footer, Header, Skills, Testimonials, Work } from './container';
 import { Navbar } from './components';
+import './App.scss';
 
 const App = () => {
   return (
