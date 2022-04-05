dcf814b4b5a60b379e9ef5517d676f0552d452cd
kartikgowda <kartik.gowda55@gmail.com>
2022-04-05 12:17:35 +1000

    Start - About Section

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
 
-import './About.scss' 
+import './About.scss';
 
 const About = () => {
   return (
