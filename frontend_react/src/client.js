8c0628ca25b0a5f445f60f1623a4bea78bdf36f1
kartikgowda <kartik.gowda55@gmail.com>
2022-04-05 17:12:00 +1000

    Adding env and Importing Sanity Images

--- frontend_react/src/client.js
+++ frontend_react/src/client.js
@@ -1,11 +1,17 @@
 import sanityClient from '@sanity/client';
-import imageBuilder from '@sanity/image-url';
+import imageUrlBuilder from '@sanity/image-url';
 
 export const client = sanityClient({
-  projectId: 'nglyj0ia',
+  projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
   dataset: 'production',
   apiVersion: '2022-04-05',
   useCdn: true,
-  token:
-    'skzNr90jl6xmzlAA9PdAXBGWcoaOgXmx18TZSPyM2DBKKV7Req1uMjfvCBGt8YzY9drsslDRjTKOaE5VzXRim5xQABA17m3XsciyUNSLb22b0ZEFqHNWOqHEl4FQ33hHTIAo2QHpy34DNzQwAlDbuyknDjIz1tRdUbfu5Fb1DjPd0HLdWZYmskzNr90jl6xmzlAA9PdAXBGWcoaOgXmx18TZSPyM2DBKKV7Req1uMjfvCBGt8YzY9drsslDRjTKOaE5VzXRim5xQABA17m3XsciyUNSLb22b0ZEFqHNWOqHEl4FQ33hHTIAo2QHpy34DNzQwAlDbuyknDjIz1tRdUbfu5Fb1DjPd0HLdWZYm',
+  token: process.env.REACT_APP_SANITY_TOKEN_ID,
 });
+
+//? For Importing Images
+const builder = imageUrlBuilder(client);
+
+//? Exporting Images
+
+export const urlFor = (source) => builder.image(source);
