3539d00cc7c9dbb54e7c8d86d10acd60c7f763fd
kartikgowda <kartik.gowda55@gmail.com>
2022-04-05 14:47:14 +1000

    Hover Effect Tween Fix!

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,12 +1,68 @@
 import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 
+//TODO : Bracket {} Understanding for Import
+import { images } from '../../constants';
 import './About.scss';
+
+const abouts = [
+  //TODO : Title & Description + Images
+  {
+    title: 'Web Development',
+    description: 'Im a batshit Developer.',
+    imgUrl: images.about01,
+  },
+  {
+    title: 'Web Design',
+    description: 'Im a matshit Developer.',
+    imgUrl: images.about02,
+  },
+  {
+    title: 'UI/UX',
+    description: 'Im a ratshit Developer.',
+    imgUrl: images.about03,
+  },
+  {
+    title: 'App Development',
+    description: 'Im a catshit Developer.',
+    imgUrl: images.about04,
+  },
+];
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <>
+      {/* //TODO : Changing the Head-text to something more useful
+       */}
+      <h2 className="head-text">
+        I Know That <span>Good Design</span>
+        <br /> means <span>Good Business</span>
+      </h2>
 
-export default About
+      <div className="app__profiles">
+        {abouts.map((about, index) => (
+          // // ! FIX ANIMATION TWEEN
+          <motion.div
+            whileInView={{ opacity: 1 }}
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.5, type: 'tween' }}
+            className="app__profile-item"
+            key={about.title + index}
+          >
+            <img src={about.imgUrl} alt={about.title} />
+            {/* //TODO : Style In CSS or Not Test
+             */}
+            <h2 className="bold-text" style={{ marginTop: 20 }}>
+              {about.title}
+            </h2>
+            <p className="p-text" style={{ marginTop: 10 }}>
+              {about.description}
+            </p>
+          </motion.div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default About;
--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-
 import { motion } from 'framer-motion';
+
 import { images } from '../../constants';
 
 import './Header.scss';
