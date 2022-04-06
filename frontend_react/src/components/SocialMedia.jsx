afb09c83c7df018e642722d89aa1ef9c0dc176d9
kartikgowda <kartik.gowda55@gmail.com>
2022-04-06 12:36:36 +1000

    Some Fixed and TODO's added

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -3,20 +3,19 @@ import { BsTwitter, BsInstagram } from 'react-icons/bs';
 import { FaFacebookF } from 'react-icons/fa';
 
 const SocialMedia = () => {
-    return (
-        <div className="app__social">
-            <div>
-                {/* //TODO : ADD SocialMedia Links */}
-                <BsTwitter />
-            </div>
-            <div>
-                <BsInstagram />
-            </div>
-            <div>
-                <FaFacebookF /> 
-            </div>
-        </div>
-    );
+  return (
+    <div className="app__social">
+      <div>
+        <BsTwitter />
+      </div>
+      <div>
+        <BsInstagram />
+      </div>
+      <div>
+        <FaFacebookF />
+      </div>
+    </div>
+  );
 };
 
 export default SocialMedia;
